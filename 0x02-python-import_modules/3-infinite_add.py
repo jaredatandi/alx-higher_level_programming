ab0085abca5654e1457bf00870bfded3828eae19
jaredatandi <jaredatandi07@gmail.com>
2022-09-25 15:21:19 +0300

    Add 3-infinite_add.py file

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+def infinite_add():
+    result = 0
+    for i in range(1, len(sys.argv)):
+        result += int(sys.argv[i])
+    print("{}".format(result))
+if __name__ == "__main__":
+    infinite_add()
