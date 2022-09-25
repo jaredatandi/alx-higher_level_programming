d0d9ab33cf5914e106103cbf5885f17bc4f66226
jaredatandi <jaredatandi07@gmail.com>
2022-09-25 15:25:17 +0300

    Fix pycode style error

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 import sys
 
+
 def infinite_add():
     result = 0
     for i in range(1, len(sys.argv)):
         result += int(sys.argv[i])
     print("{}".format(result))
+
+
 if __name__ == "__main__":
     infinite_add()
