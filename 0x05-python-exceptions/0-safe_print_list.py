aa66e68505a768edc39fe9cded25f1cba4509cd7
jaredatandi <jaredatandi07@gmail.com>
2022-10-11 19:20:16 +0300

    Refactor code

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     i = 0
-    try:
-        while(i < x):
-            print("{}".format(my_list[i]), end="");
-            i += 1
-    except:
-        pass
+    while (i < x):
+        try:
+            print("{}".format(my_list[i]), end="")
+        except Exception:
+            pass
+        i += 1
     print()
     return (i)
