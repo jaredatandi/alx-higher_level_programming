c53ed4dfbb6ec1bc08267204bb3677ff716b2047
jaredatandi <jaredatandi07@gmail.com>
2022-10-11 22:32:59 +0300

    Remove pycodestyle errors

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list, x=0):
-    """description: Docstring for safe_print_list_integers 
-    :returns: number of printed elements 
+    """description: Docstring for safe_print_list_integers
+    :returns: number of printed elements
     """
     c = 0
     for i in range(0, x):
