6c04cf92a59e8fd4593e21a0e1cf1b7abb06d4b9
jaredatandi <jaredatandi07@gmail.com>
2022-10-11 18:46:12 +0300

    Handle range iteration in the list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    i = 0
     try:
-        i = 0
         while(i < x):
-            for i in my_list:
-                print("{}".formart(i));
+            print("{}".format(my_list[i]), end="");
             i += 1
-    except IndexError:
-        print("Out of range")
+    except:
+        pass
+    print()
     return (i)
