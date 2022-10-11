c9a8ad8e939c9d9a9f9ddc0fc06097394a84537e
jaredatandi <jaredatandi07@gmail.com>
2022-10-11 20:57:35 +0300

    Add 2-safe_print_list_integers.py file

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list, x=0):
+    """TODO: Docstring for .
+    :returns: TODO
+    """
+    i = 0
+    try:
+       while (i < x):
+           print("{:d}".format(my_list[i]), end="")
+           i += 1
+    except Exception as e:
+        pass
+    print()
+    return (i)
