ff6003fb7a85f6ce2640a177d5a335257c597c57
jaredatandi <jaredatandi07@gmail.com>
2022-10-11 21:01:27 +0300

    Change indentation space

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,9 +5,9 @@ def safe_print_list_integers(my_list, x=0):
     """
     i = 0
     try:
-       while (i < x):
-           print("{:d}".format(my_list[i]), end="")
-           i += 1
+        while (i < x):
+            print("{:d}".format(my_list[i]), end="")
+            i += 1
     except Exception as e:
         pass
     print()
