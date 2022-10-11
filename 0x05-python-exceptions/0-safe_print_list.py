8a35ea18a8e10298a4201fe987653d00f1ceec55
jaredatandi <jaredatandi07@gmail.com>
2022-10-11 19:22:29 +0300

    Handle bare exception warning

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     i = 0
-    while (i < x):
-        try:
+    try:
+        while (i < x):
             print("{}".format(my_list[i]), end="")
-        except Exception:
-            pass
-        i += 1
+            i += 1
+    except Exception:
+        pass
     print()
     return (i)
