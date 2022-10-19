93976d8debf19df0cea9d18151dbd467717817b8
jaredatandi <jaredatandi07@gmail.com>
2022-10-19 21:03:34 +0300

    Update documentation

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,15 +1,30 @@
 #!/usr/bin/python3
+""" Module 1-Rectangle
+Defines a Rectangle class.
+"""
+
+
 class Rectangle:
+    """Rectangle class defined by height and width"""
+
     def __init__(self, width=0, height=0):
+        """Intializes a Rectangle isinstance.
+        
+        Args:
+            width: the width measurement
+            height: the height measurement
+        """
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """Gets the width of the Rectangle isinstance"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Sets the width of the Rectangle isinstance"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -18,10 +33,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """Gets the height of the Rectangle isinstance"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Sets the height of the Rectangle isinstance"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
