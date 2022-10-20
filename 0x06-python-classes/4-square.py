6a2baf90e12aacaf245003c7d80cebd74db8fd7f
jaredatandi <jaredatandi07@gmail.com>
2022-10-20 19:56:21 +0300

    Include setters and getters for the class

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Defines a class square with
+    Private instance attribute size
+    Instatiation with optional size
+    An area method
+    """
+
+    def __init__(self, size=0):
+        """Intializes the class Square"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Gets the size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size to the given value"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculates the area using the size"""
+        return self.__size * self.__size
