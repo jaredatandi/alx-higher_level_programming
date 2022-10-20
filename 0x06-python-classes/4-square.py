e1607a015844e84e9b39bfca3219013540817b34
jaredatandi <jaredatandi07@gmail.com>
2022-10-20 20:40:19 +0300

    Handle private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -22,9 +22,9 @@ class Square:
     @size.setter
     def size(self, value):
         """Sets the size to the given value"""
-        if not isinstance(size, int):
+        if not isinstance(self.__size, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif self.__size < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
