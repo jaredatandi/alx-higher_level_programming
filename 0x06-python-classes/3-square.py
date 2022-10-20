3e4065fef37c562ff9855e740060a2c3b1c6b5e1
jaredatandi <jaredatandi07@gmail.com>
2022-10-20 19:48:31 +0300

    Add a method to get the area of the square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Defines a class square with
+    Private instance attribute size
+    Instatiation with optional size
+    An area method
+    """
+
+
+    def __init__(self, size=0):
+        """Intializes the class Square"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Calculates the area using the size"""
+        return self.__size * self.__size
