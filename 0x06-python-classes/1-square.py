6e617e63107ae4501aef494a9ccbc67d8fad9538
jaredatandi <jaredatandi07@gmail.com>
2022-10-20 20:21:39 +0300

    Fix pycodestyle errors

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,13 +2,11 @@
 """Represents a square"""
 
 
-
 class Square:
     """Represents a square.
     Private instance attribute: size.
     Instantiation with size (no type/value verification).
     """
-
 
     def __init__(self, size):
         """Intiliazes the size"""
