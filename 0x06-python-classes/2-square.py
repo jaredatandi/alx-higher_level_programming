7a09ebeab14f6e33252a61ddf95b91f3063873b9
jaredatandi <jaredatandi07@gmail.com>
2022-10-20 20:22:13 +0300

    Remove whitespace

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
+"""Represents a square class"""
+
+
 class Square:
     """Represents a square class.
     Private instance attribute: size.
-    With type and negative value checking 
+    With type and negative value checking
     """
-
 
     def __init__(self, size=0):
         """Intiliazes the size"""
-        if not isinstance(size, int): 
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
