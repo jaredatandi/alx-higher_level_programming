634cb8c6b6ff8c93eb7ff0105a8bcfa6446c70fc
jaredatandi <jaredatandi07@gmail.com>
2022-10-24 18:18:19 +0300

    Code refactor

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
-"""Module 7-base_geometry.
-Creates a BaseGeometry class.
+"""Module Base class 
+defines a class BaseGeometry
 """
-
-
 class BaseGeometry:
-    """A class with a public method and a validator."""
+    """A class representation"""
 
     def area(self):
-        """A class that raise an Exception with the message
-        'area() is not implemented'
-        """
+        """Yet to be implemented method"""
 
-        raise Exception('area() is not implemented')
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """A method to validate a value
+        """Validates parameters
 
         Args:
-            name: a string name of the parameter
-            value: the parameter to validate
+            - name: the name of the parameter
+            - value: the parameter value
+        Raise:
+            TypeError: if value not an int
+            ValueError: if value is less than or equal to 0
         """
-
-        if type(value) != int:
+        
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
-
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
