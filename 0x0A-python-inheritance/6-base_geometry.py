e616410bf94463eac628554d44d68fe16f3ce656
jaredatandi <jaredatandi07@gmail.com>
2022-10-24 16:23:56 +0300

    Add a public instance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module 5-base_geometry
+A class based on an empty class
+"""
+
+
+class BaseGeometry:
+    """A class with a public method."""
+
+    def area(self):
+        """A class that raise an Exception with the message
+        'area() is not implemented'
+        """
+
+        raise Exception('area() is not implemented')
