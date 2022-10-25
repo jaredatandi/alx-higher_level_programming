b92166fdec6b762988b77d3ef56fc5b4d7f6832c
jaredatandi <jaredatandi07@gmail.com>
2022-10-25 09:58:36 +0300

    Import module rather than implement it

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -4,29 +4,7 @@ defines a class BaseGeometry
 """
 
 
-class BaseGeometry:
-    """A class representation"""
-
-    def area(self):
-        """Yet to be implemented method"""
-
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validates parameters
-
-        Args:
-            - name: the name of the parameter
-            - value: the parameter value
-        Raise:
-            TypeError: if value not an int
-            ValueError: if value is less than or equal to 0
-        """
-
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
