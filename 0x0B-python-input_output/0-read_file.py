0df2f4abe1c75ce89d262665fcf3993ab3fb6940
jaredatandi <jaredatandi07@gmail.com>
2022-10-26 00:29:11 +0300

    Change the input name to the function

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,10 +7,10 @@ Reads from a text file and prints to stdout
 def read_file(filename=""):
     """Reads from a filename and prints the content
 
-    Args: 
+    Args:
         filename: the name of the file to open
     """
 
-    with open("filename", encoding="utf-8") as file:
+    with open(filename, encoding="utf-8") as file:
         read_data = file.read()
         print(read_data, end="")
