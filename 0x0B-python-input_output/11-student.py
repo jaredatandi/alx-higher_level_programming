0960d2de9d46f9bbadc2430469907f1821d6dc8e
jaredatandi <jaredatandi07@gmail.com>
2022-10-26 01:36:40 +0300

    Reload from JSON

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Module 11-student.
+A class Student
+"""
+
+
+class Student:
+    """Defines a class student
+    Public attr:
+        first_name
+        last_name
+        age
+    Public method to_json().
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes the class"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dict representation of a student
+        Returns:
+            the dict representation of the instance
+        """
+
+        my_dict = dict()
+        if type(attrs) is list and all(type(x) is str for x in attrs):
+            for x in attrs:
+                if x in self.__dict__:
+                    my_dict.update({x: self.__dict__[x]})
+            return my_dict
+        return self.__dict__.copy()
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance
+
+        Args:
+            json: dict of attributes
+        """
+
+        for elem in json:
+            self.__dict__.update({x: json[x]})
