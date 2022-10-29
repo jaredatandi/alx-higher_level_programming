d0e20f3102facb489c728940bdfec13c7668b7e7
jaredatandi <jaredatandi07@gmail.com>
2022-10-29 15:05:59 +0300

    Use provided test

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Module base class
+Creates a base class
+id is always an int
+"""
+
+
+class Base:
+    """Base class
+
+    Private attrs:
+        __nb__objects: 0
+    public instance:
+        id
+    """
+
+    __nb__objects = 0
+
+    def __init__(self, id=None):
+        """initializes the class
+
+        Args:
+            id: class id assignment
+        """
+
+        self.id = 0
+        if id == None:
+            Base.__nb__objects += 1
+            self.id = Base.__nb__objects
+        else:
+            self.id = id
