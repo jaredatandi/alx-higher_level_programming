c68836c7dc8736cd99aa57189d11601cb4d52b69
jaredatandi <jaredatandi07@gmail.com>
2022-12-19 22:57:04 +0300

    Use loops

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*
+ * print a simple message using a const variable
+ * to the console log
+ */
+
+const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (const message of messages) {
+  console.log(message);
+}
