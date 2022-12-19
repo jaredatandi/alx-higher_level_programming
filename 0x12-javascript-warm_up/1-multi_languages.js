12354c8d3a987ed8cc16603946017c8d1392cf0b
jaredatandi <jaredatandi07@gmail.com>
2022-12-19 22:05:20 +0300

    Use multilines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/*
+ * print a simple message using a const variable
+ * to the console log
+ */
+
+const myVar = 'C is fun\n' +
+'Python is cool\n' +
+'JavaScript is amazing';
+console.log(myVar);
