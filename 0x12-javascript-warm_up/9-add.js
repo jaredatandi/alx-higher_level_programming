91cf691f2db9d8b4781c174b0bd285efeffbe9a4
jaredatandi <jaredatandi07@gmail.com>
2022-12-20 21:13:11 +0300

    Remove linters erros

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // Script to add
 
-let num1 = parseInt(process.argv[2]);
-let num2 = parseInt(process.argv[3]);
-let result = num1 + num2;
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
+const result = num1 + num2;
 
 console.log(result);
