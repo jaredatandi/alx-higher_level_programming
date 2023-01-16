39f20e8a0d5661a360ccd1c874ec24bd5429d149
jaredatandi <jaredatandi07@gmail.com>
2023-01-16 18:53:28 +0300

    Reduce the characters to less than 79 on a line

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -12,7 +12,8 @@ def find_peak(list_of_integers):
         return max(list_of_integers)
 
     mid = len(list_of_integers) // 2
-    if list_of_integers[mid] > list_of_integers[mid - 1] and list_of_integers[mid] > list_of_integers[mid + 1]:
+    if list_of_integers[mid] > list_of_integers[mid - 1] and \
+            list_of_integers[mid] > list_of_integers[mid + 1]:
         return list_of_integers[mid]
     elif list_of_integers[mid - 1] > list_of_integers[mid]:
         return find_peak(list_of_integers[:mid])
