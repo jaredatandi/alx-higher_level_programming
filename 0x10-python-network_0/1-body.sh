afa297cfff5999aac34ac75387ca52f42d507290
jaredatandi <jaredatandi07@gmail.com>
2023-01-16 13:08:13 +0300

    Update the script

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 # Script that takes URL, sends a request, and displays the size of the body
 url=$1
-response=$(curl -s -o /dev/null -w "%{http_code}" $url)
-
-# Check for the code
-if [ $response -eq 200 ]; then
-	# show the body
-	curl -s $url
-else
-	# Display error message if status code is not 200
-	echo "Error: Response status code is $response"
-fi
+curl -sL url
