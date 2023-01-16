dcab88d306defd06873e36fbc96c611d5adccbb0
jaredatandi <jaredatandi07@gmail.com>
2023-01-16 18:47:51 +0300

    Peak finding function and test case

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" A function to find a peak in a list of unsorted ints
+"""
+
+
+def find_peak(list_of_integers):
+    if not list_of_integers:
+        return None
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+    if len(list_of_integers) == 2:
+        return max(list_of_integers)
+
+    mid = len(list_of_integers) // 2
+    if list_of_integers[mid] > list_of_integers[mid - 1] and list_of_integers[mid] > list_of_integers[mid + 1]:
+        return list_of_integers[mid]
+    elif list_of_integers[mid - 1] > list_of_integers[mid]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
