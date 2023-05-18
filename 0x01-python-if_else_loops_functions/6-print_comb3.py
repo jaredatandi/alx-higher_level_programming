c54d7c2437ff8b1484a80a33536ac1d042800b84
jaredatandi <jaredatandi07@gmail.com>
2023-05-18 18:15:18 +0300

    update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 for i in range(0, 9):
     for j in range(0, 10):
-        if i > j:
-            pass
-        elif f'{i}{j}' == '89':
+        if i >= j:
+            continue
+        elif i == 8 and j == 9:
             print(f'{i}{j}')
         else:
             print(f'{i}{j}', end=", ")
