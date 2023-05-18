86cdb1d633f0c606d0447cadbb3fbb629ef401ef
jaredatandi <jaredatandi07@gmail.com>
2023-05-18 22:10:41 +0300

    update 7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
-    """TODO: checks a char for lowercase 
-    :returns: True or False 
+    """TODO: checks a char for lowercase
+    :returns: True or False
 
     """
     b = ord(c)
