00b3b21db693ed65c661d42e6fa4e2a0033ed823
jaredatandi <jaredatandi07@gmail.com>
2023-05-18 22:09:58 +0300

    Implement islower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
+def islower(c):
+    """TODO: checks a char for lowercase 
+    :returns: True or False 
 
+    """
+    b = ord(c)
+    if b in range(ord('a'), ord('z')):
+        return True
+    else:
+        return False
