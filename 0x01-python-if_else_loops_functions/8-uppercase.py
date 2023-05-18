a325c35340db6b65f8b16f9fa406c848d09e5e52
jaredatandi <jaredatandi07@gmail.com>
2023-05-18 22:24:38 +0300

    Add 8-uppercase.py file

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uppercase(str):
+    """TODO: Docstring for is uppercase.
+    :returns: TODO
+
+    """
+    for i in str:
+        ascii_value = ord(i)
+        if 97 <= ascii_value <= 122:
+            ascii_value -= 32
+        print(chr(ascii_value), end='')
+    print()
